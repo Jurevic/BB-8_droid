6fe3c24d899b45b288b96cabe5823c35c213fd72
Povilas V. Dudonis <povilas.dudonis@gmail.com>
2017-04-23 20:18:50 +0300

    Added video stram html draft, url to video stream hmtl and created draft view

--- bb8/users/views.py
+++ bb8/users/views.py
@@ -46,3 +46,10 @@ class UserListView(LoginRequiredMixin, ListView):
     # These next two lines tell the view to index lookups by username
     slug_field = 'username'
     slug_url_kwarg = 'username'
+
+
+class VideoDetailView(LoginRequiredMixin, ListView):
+    model = User
+    # These next two lines tell the view to index lookups by username
+    slug_field = 'username'
+    slug_url_kwarg = 'username'
